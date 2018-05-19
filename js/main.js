13f010a149c749b84ca18db3ea0beec51509cb9d
QianYuanjia <qianyuanjia@qq.com>
2018-05-19 16:37:19 +0800

    edit

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ window.onload=function(){
 				oCtx.strokeStyle=color;
 				oShowLw.style.background=color;
 			}
-			newImg.src='//i.loli.net/2018/05/19/5affdf6b90753.png';
+			newImg.src='../images/color.png';
 			oColor.className='';
 		}
 		
